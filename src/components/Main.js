8cee0d8abdba0ae75b9ebb65d9f85d6f55156d0e
Joseph <117396146+joeytraffic@users.noreply.github.com>
2023-09-12 13:48:53 -0700

    working about section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,15 @@
+import cloud1 from "../images/clouds/cloud1.png";
+import cloud2 from "../images/clouds/cloud2.png";
+import cloud3 from "../images/clouds/cloud3.png";
+
 const Main = () => {
   return (
-    <div>
+    <div className="main-container">
+      <div className="clouds">
+        <img className="cloud cloud1" src={cloud1} alt="Cloud 1" />
+        <img className="cloud cloud2" src={cloud2} alt="Cloud 2" />
+        <img className="cloud cloud3" src={cloud3} alt="Cloud 3" />
+      </div>
       <div className="about">
         <span className="nameBig">Joey Tranvik</span>
         <p className="aboutSmall">
