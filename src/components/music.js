5995c2dff56d43c174d3bf4daaba5ef01d7159dd
Joey Traffic <joeytranvik@Joeys-MacBook-Pro-2.local>
2023-09-28 12:10:40 -0700

    added carousel for the music

--- src/components/music.js
+++ src/components/music.js
@@ -1,43 +1,72 @@
+import { useState } from 'react';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 const Music = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+  };
+
+  const musicItems = [
+    {
+      src:
+        'https://open.spotify.com/embed/album/7zswj77WxeI6wkUcCdwoMq?utm_source=generator&theme=0',
+      height: '152',
+    },
+    {
+      src: 'https://open.spotify.com/embed/album/5gGBHy2wYWJiBEgWUgf1qA?utm_source=generator',
+      height: '352',
+    },
+    {
+      src: 'https://open.spotify.com/embed/album/1nUNpDFO1ButAVpIaRWaYs?utm_source=generator',
+      height: '352',
+    },
+  ];
+
+  const handleIframeLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
-    <div>
-      {/* create carousel of music here */}
+    <div className="music-main">
       <div className="music-container">
-        <div className="music">
-          <div className="music-item">
-            <iframe
-              src="https://open.spotify.com/embed/album/7zswj77WxeI6wkUcCdwoMq?utm_source=generator&theme=0"
-              width="80%"
-              height="152"
-              frameBorder="0"
-              allowfullscreen=""
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              loading="lazy"
-            ></iframe>
+        {isLoading && (
+          <div class="lds-facebook">
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
-          <div className="music-item">
-            <iframe
-              src="https://open.spotify.com/embed/album/5gGBHy2wYWJiBEgWUgf1qA?utm_source=generator"
-              width="80%"
-              height="352"
-              frameBorder="0"
-              allowfullscreen=""
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              loading="lazy"
-            ></iframe>
-          </div>
-          <div className="music-item">
-            <iframe
-              src="https://open.spotify.com/embed/album/1nUNpDFO1ButAVpIaRWaYs?utm_source=generator"
-              width="80%"
-              height="352"
-              frameBorder="0"
-              allowfullscreen=""
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              loading="lazy"
-            ></iframe>
-          </div>
-        </div>
+        )}
+        <Slider {...settings}>
+          {musicItems.map((item, index) => (
+            <div key={index} className="music-item">
+              <div className="iframe-wrapper">
+                <iframe
+                  src={item.src}
+                  width="80%"
+                  height={item.height}
+                  frameBorder="0"
+                  allowFullScreen
+                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                  loading="auto"
+                  onLoad={handleIframeLoad}
+                  style={{ display: isLoading ? 'none' : 'block' }}
+                ></iframe>
+              </div>
+            </div>
+          ))}
+        </Slider>
+        <div className="mask-overlay left"></div>
+        <div className="mask-overlay right"></div>
       </div>
     </div>
   );
